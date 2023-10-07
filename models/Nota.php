a36b1648f6c0a7e30672459a7917576cc7a24b59
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-07 07:33:55 -0600

    Creación de los modelos

---
+++ models/Nota.php
@@ -0,0 +1,13 @@
+<?php
+require_once('../config/db.php');
+
+class Nota {
+    private $conexion;
+
+    public function __construct() {
+        $this->conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if ($this->conexion->connect_error) {
+            die("Error al conectar a la base de datos: " . $this->conexion->connect_error);
+        }
+    }
+}
